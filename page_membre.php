0136a321c8aea243b9074170da4598599964863b
xavier <maubeuge.dwwm@gmail.com>
2020-09-25 11:18:39 +0200

    annonce + immo

--- page_membre.php
+++ page_membre.php
@@ -4,13 +4,6 @@ include ( 'L_fonctions_generales.php');
 setHeaderNoCache();
 gestionSession();
 
-$user_obj  = $_SESSION['user'];
-$users = $_SESSION['users'];
-
-echo "Votre login est ".$user_obj->pseudo."<br>\n";
-
-// On affiche un lien pour fermer notre session
-echo '<a href="./logout.php">Déconnection</a><br>';
 	 	
 ?>
 <?php
