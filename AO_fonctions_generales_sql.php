bf5732a85a18de9ac44285fa879c3b330c1dc3e1
Maxime <maximewilmot@gmail.com>
2020-10-06 11:51:41 +0200

    3eme jour11h30

--- AO_fonctions_generales_sql.php
+++ AO_fonctions_generales_sql.php
@@ -30,7 +30,33 @@ class Annonce
 
 	public function save()
 	{
-	
+        
+
+        $servername = "10.115.49.73";
+        $username = "maxime";
+        $password = "maxime";
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+        
+        // Check connection
+        if ($conn->connect_error) 
+        {
+          die("Connection failed: " . $conn->connect_error);
+        }
+        
+        echo "Connected successfully";
+        
+        
+        $sql = "INSERT INTO maxime.annonces ( title, description, image, prix )  VALUES ('title','description','image', 'prix' )";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully<br>";
+          } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+          }
+          
+          $conn->close();
 	}
 
 
