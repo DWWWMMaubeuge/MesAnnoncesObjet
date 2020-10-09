fb6c4168ef602bba2d3a8825764c29414c7e23c3
nicolas <nicolascaulier@gmail.com>
2020-10-09 08:13:40 +0200

    ok

--- SQL/createTable.php
+++ SQL/createTable.php
@@ -1,50 +1,8 @@
 <?php
-<<<<<<< HEAD
-	$servername = "10.115.49.73";
-	$username = "nicolas";
-	$password = "nicolas";
-=======
->>>>>>> 31095d3d5ec04c48d778987f629c567695329ea7
 
 	include ( 'AO_fonctions_generalesSQL.php');
 
 
-<<<<<<< HEAD
-	//$req = "CREATE TABLE nicolas.annonce (  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, titre varchar(255),  description varchar(255),  image varchar(255), prix INT );";
-	
-	echo $req."<br>";
-	$result = $conn->query( $req );	
-	$conn->close();
-
-	if ( $_POST )
-	{
-		$titre = "";
-		$description ="";
-		$image ="";
-		$prix = "";
-
-		$titre = $_POST["titre"];
-
-		$description = $_POST["description"];
-
-		$image = $_POST["image"];
-
-		$prix = $_POST['prix'];
-
-		if ( $titre == "" ){
-			die("S'il vous plaît entrez votre titre");
-		  }
-		if ( $description == "" ){
-			die("S'il vous plaît entrez votre description");
-		  }
-		  if ( $image == "" ){
-			die("S'il vous plaît entrez votre image");
-		  }
-		  if ( $prix == "" ){
-			die("S'il vous plaît entrez votre prix");
-		  }
-	}
-=======
 
 	echo "creation de la table annonces<br>";
 	$req = "CREATE TABLE xavier.annonces (  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, titre varchar(255),  description varchar(255),  image varchar(255), prix INT );";
@@ -56,7 +14,6 @@
 
 
 
->>>>>>> 31095d3d5ec04c48d778987f629c567695329ea7
 ?>
 <html>
   <head>
