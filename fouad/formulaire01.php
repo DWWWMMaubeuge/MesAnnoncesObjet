56a366f53ffe6643ad7a2b3f2012d5e8f582bc46
stev <jeffrey_stl@hotmail.fr>
2020-10-13 08:33:22 +0200

    REPRISE

--- fouad/formulaire01.php
+++ fouad/formulaire01.php
@@ -5,8 +5,8 @@
 function SQLQuerry( $req )
 {
     $servername = "10.115.49.73";
-    $username = "xavier";
-    $password = "xavier";
+    $username = "steven";
+    $password = "steven";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password);
@@ -23,7 +23,10 @@ function SQLQuerry( $req )
 
   if ( $_POST ) 
   {
-    $name  = ""; 
+    $name  = "";
+    $surname = "";
+    $age = "";
+    $dep = "";
     $email = ""; 
 
     //if ( isset( $_POST["Name"])  )
@@ -31,10 +34,29 @@ function SQLQuerry( $req )
     
     //if ( isset( $_POST["Email"])  )
       $email = $_POST["Email"];
+
+     //if ( isset( $_POST["Email"])  )
+      $surname = $_POST["Surname"];
+
+    //if ( isset( $_POST["Name"])  )
+      $age = $_POST["Age"];
+
+    //if ( isset( $_POST["Name"])  )
+      $dep = $_POST["Dep"];
     
     if ( $name == "" )
     {
       die("S'il vous plaît entrez votre nom");
+    }
+
+    if ( $surname == "" )
+    {
+      die("S'il vous plaît entrez votre prénom");
+    }
+
+    if ( $age == "" )
+    {
+      die("S'il vous plaît entrez votre age");
     }
     
     if ( $email == "" || !filter_var($email, FILTER_VALIDATE_EMAIL))
@@ -42,10 +64,16 @@ function SQLQuerry( $req )
       die("S'il vous plaît entrez votre adresse e-mail");
     }
 
-    print "Salut " . $name . "!, votre adresse e-mail est ". $email."<br>";
+        if ( $dep == "" )
+    {
+      die("S'il vous plaît entrez votre departement");
+    }
 
+    print "Salut " . $surname . "!, votre adresse e-mail est ". $email."<br>";
 
-    $req = "INSERT INTO xavier.form01 (nom, email ) VALUES ( '$name', '$email' );";
+    $req = "ALTER TABLE steven.form01 ADD dep INT ";
+    SQLQuerry( $req );
+    $req = "INSERT INTO steven.form01 (name, surname, age, email, dep ) VALUES ( '$name', '$surname', '$age', '$email', '$dep' );";
     SQLQuerry( $req );
   }
 ?>
@@ -57,7 +85,10 @@ function SQLQuerry( $req )
   <body>
     <form method="post" action="formulaire01.php">
       <input type="text" name="Name" placeholder="Entrez votre nom" /><br />
+      <input type="text" name="Surname" placeholder="Entrez votre prénom" /><br />
+      <input type="INT" name="Age" placeholder="Entrez votre age" /><br />
       <input type="email" name="Email" placeholder="Entrez votre Email" /><br />
+      <input type="INT" name="Dep" placeholder="Entrez votre departement" /><br />
       <input type="submit" value="Submit" />
     </form>
   </body>
