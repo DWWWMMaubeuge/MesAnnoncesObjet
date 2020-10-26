c7227584b2f3bee604759f31d7db4e83370a5209
DWWWMMaubeuge <maubeuge.dwwm@gmail.com>
2020-10-26 14:56:18 +0100

    exo tableau 3 version Corrigé 1

--- POO/object01.php
+++ POO/object01.php
@@ -1,9 +1,28 @@
 <?php
 
 $tableau = [
-                [ "1.1", "1.2"],
-                [ "2.1", "2.2"]
+    [ "1.1", "1.2"],
+    [ "2.1", "2.2"],
+    [ "3.1", "3.2"]
             ]; 
+
+
+// 1.1   |  1.2   |
+// 2.1   |  2.2   |
+
+
+echo "<table border=\"solid\">\n";
+for( $ligne=0 ; $ligne < count(  $tableau ) ; $ligne++ )
+{
+    echo "<tr>\n";
+    $ligneTab = $tableau[ $ligne ];
+    for( $colone=0 ; $colone < count(  $ligneTab ) ; $colone++ )
+        echo "<td>".$ligneTab[$colone]."</td>\n";
+
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
 
 
 
