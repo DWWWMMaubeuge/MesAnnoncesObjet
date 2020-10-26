cd9d15c614992b700bb43a78d8b14ccb98fc76d6
DWWWMMaubeuge <maubeuge.dwwm@gmail.com>
2020-10-26 14:21:43 +0100

    exo tableau 3 version

--- POO/object02exo.php
+++ POO/object02exo.php
@@ -59,10 +59,48 @@ function afficheAnimal(  $num )
     echo $num." ".$animal[0]." -> ".$animal[ 1 ]."<br>";
 }
 
+function afficheAnimaux(  $tab )
+{
+    foreach( $animaux as $animal )
+    {
+        echo $animal[0]." -> ".$animal[ 1 ]."<br>";
+    } 
+}
+
+function afficheAnimaux2(  $tab )
+{
+    for( $a=0; $a < count( $tab ) ; $a++ )
+    {
+        afficheAnimal(  $a );
+    } 
+}
+
 function afficheAnimalByEspece(  $espece )
 {
-    GLOBAL $animaux, $cris;
-    
+    GLOBAL $animaux;
+
+    foreach( $animaux as $animal )
+    {
+        if ( $animal[0] == $espece )
+        {
+            echo $animal[0]." -> ".$animal[ 1 ]."<br>";
+            break;
+        }
+    } 
+}
+
+function getIDAnimalByEspece(  $espece )
+{
+    GLOBAL $animaux;
+
+    for( $ligne=0 ; $ligne < count($animaux) ; $ligne++ )
+    {
+        if ( $animaux[$ligne][0] == $espece )
+        {
+            return $ligne;
+        }
+    } 
+    return -1;
 }
 
 for( $a=0; $a < count( $animaux ) ; $a++ )
@@ -71,22 +109,73 @@ for( $a=0; $a < count( $animaux ) ; $a++ )
 } 
 
 
+afficheAnimaux( $animaux );
+
+echo "<br>";
+
+
 afficheAnimalByEspece( 'chat');
 // 1 chat -> miaule
+
+
+$id = getIDAnimalByEspece( 'chat');
+echo "$id<br>";
+afficheAnimal(  $id );
+
+echo "<br><br>";
+
+$id = getIDAnimalByEspece( 'oiseau');
+echo "$id<br>";
+afficheAnimal(  $id );
 
 //afficheAnimal(  0 );
 // 0 chien -> aboie
 
 
-/*
-echo "<br> Version Objet<br>";
+echo "<br><br> Version Objet<br>";
 
 class Animal
 {
+    public $espece;
+    public $cri;
+
+    public function __construct()
+    {
+        $this->espece = "rien";
+        $this->cri = "rien";
+    }
+
+    public function affiche()
+    {
+        echo $this->espece." -> ".$this->cri."<br>";
+    }
 }
 
 class Chien extends Animal
 {
+    public function __construct()
+    {
+        $this->espece = "chien";
+        $this->cri = "aboie";
+    }
+}
+
+class Chat extends Animal
+{
+    public function __construct()
+    {
+        $this->espece = "chat";
+        $this->cri = "miaule";
+    }
+}
+
+class Poisson extends Animal
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->espece = "poisson";
+    }
 }
 
 
@@ -99,8 +188,8 @@ $poisson = new Poisson();
 $listeAnimaux = [ $chien, $chat, $poisson   ];
 
 foreach(  $listeAnimaux as $anim )
-    $anim->cri();
+    $anim->affiche();
 
-*/
+
 
 ?>
