a14f69f1b6d02dca3a20034de31d62aa7eeb9835
Taf-git <fatformationafpa@gmail.com>
2020-10-26 14:32:34 +0100

    save before push

--- POO/object01.php
+++ POO/object01.php
@@ -5,26 +5,67 @@
 // 1 chat -> miaule
 // 2 poisson -> rien
 
-
+//tableau boucle for
 $animaux = [ 'chien', 'chat', 'poisson' ];
 $cris = [ 'aboie', 'miaule', 'rien' ];
 
-
+//tableau boucle foreach
+$animals=array('chien'=>'aboie', 'chat'=>'miaule','poisson'=>'rien');
 
 echo "<br> Version a l'arrache<br>";
+
+for( $i=0 ; $i < count($animaux) ; $i++ )
+        echo "$i ->".$animaux[$i]." ".$cris[$i]."<br>";
+        
 
 
 echo "<br> Version Procedurale (ou fonctionnelle)<br>";
 
-function afficheAnimal(  $num )
+echo "boucle for<br>";
+
+
+function afficheAnimal( $tab )
 {
     GLOBAL $animaux, $cris;
+	for( $i=0 ; $i < count($tab) ; $i++ )
+	echo "$i ->".$animaux[$i]." ".$cris[$i]."<br>";
 }
 
 
-afficheAnimal(  0 );
+afficheAnimal($animaux);
+echo "<br>";
+
+echo "boucle foreach <br>";
+
+
+function callArray($array)
+{
+        foreach ($array as $key=>$values)
+        {  
+            echo $key."-> ".$values." "."<br>";  
+        }       
+}
+
+callArray($animals);
+
+//afficheAnimal(  0 );
 // 0 chien -> aboie
 
+function afficheAnimal2( $espece )
+{
+    GLOBAL $animals;
+   
+    foreach ($animals as $key)
+    {  if ($animals == $espece)
+        {
+            echo $animals[0]."->".$animals[1]."<br>";
+        break;
+        }
+    }     
+
+}
+
+afficheAnimal2('chat');
 
 echo "<br> Version Objet<br>";
 
